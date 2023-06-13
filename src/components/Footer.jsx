fc267c96fcf3bf4c0de28c06d26b0895232ae747
Chadi SABA <chasaba@WKMZT81568FB.global.publicisgroupe.net>
2023-06-13 17:49:06 +0200

    refacto add product footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,14 +10,43 @@ export default function Footer() {
         position: 'fixed',
         left: 0,
         bottom: 0,
-        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
       }}
     >
-      {' '}
-      <Typography variant='body2' color='text.secondary' align='center'>
-        {'Copyright ©'} {new Date().getFullYear()}
-      </Typography>
-      <img src={dpdLogo} alt='dpd logo' style={{ width: 80, height: 80 }} />
+      <div>
+        {' '}
+        <img src={dpdLogo} alt='dpd logo' style={{ width: 80, height: 80 }} />
+      </div>
+      <div>
+        <lu>
+          <li>
+            <a href='#'>
+              <str>Need help ? </str>
+            </a>
+          </li>
+          <li> Consignees FAQ </li>
+          <li>Shippers FAQ</li>
+          <li>Prepare your shipments</li>
+          <li>Contact us</li>
+        </lu>
+      </div>
+      <div>
+        {' '}
+        <lu>
+          <li>
+            <a href='#'>
+              <str>Join us </str>
+            </a>
+          </li>
+          <li> Working at DPD France</li>
+          <li>Our positions</li>
+          <li>Your application</li>
+          <li>Our job opportunities</li>
+        </lu>
+      </div>
     </footer>
   );
 }
